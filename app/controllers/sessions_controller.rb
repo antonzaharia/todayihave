920a62cc1ffff82db4531d1edfd6ea9840aa76d5
antonzaharia <trialuser@Yozus-iMac.local>
2022-12-07 10:18:04 +0000

    fixed sessions create action tests

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    @user = User.find_by(email: user_params[:email])
+
+    if @user && @user.authenticate(user_params[:password])
+      session[:user_id] = @user.id
+    else
+      redirect_to new_sessions_path, message: 'Wrong credentials', status: :unauthorized
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
 end

7534bda021e44d783484565bd42a08df2e52dc3c
antonzaharia <trialuser@Yozus-iMac.local>
2022-12-07 11:37:47 +0000

    fixed posts unit tests

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
 
+  validates_uniqueness_of :title, conditions: -> { where('DATE(created_at) = ?', Date.today) }, message: 'has already been taken today'
+  validates :title, presence: true
+  validates :content, presence: true
 end

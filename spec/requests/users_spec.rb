fb43c9eb5878a1c92e62550bea59647c5206afb2
antonzaharia <trialuser@Yozus-iMac.local>
2022-12-07 10:03:20 +0000

    added and tested user validations

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,11 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  describe "GET /new" do
-    it "returns http success" do
-      get "/users/new"
-      expect(response).to have_http_status(:success)
+  describe 'POST /create' do
+    let(:params) do
+      {
+        user: {
+          username: Faker::Internet.username,
+          email: Faker::Internet.email,
+          password: Faker::Internet.password
+        }
+      }
+    end
+
+    it 'creates a new user' do
+      post '/users', params: params
+
+      expect(session[:user_id]).to eq(assigns(:user).id)
+      expect(User.first.username).to eq(params[:user][:username])
+      expect(User.first.email).to eq(params[:user][:email])
+      expect(User.first.authenticate(params[:user][:password])).to eq(User.first)
+    end
+
+    it 'validates the new user' do
+      FactoryBot.create(:user, username: params[:user][:username])
+      post '/users', params: params
+
+      expect(response.status).to eq(422)
+      expect(assigns(:user).errors.first.full_message).to eq('Username has already been taken')
     end
   end
-
 end

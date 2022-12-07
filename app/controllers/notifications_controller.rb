e40cb363abf8c816df49711c5d83415aff06b732
antonzaharia <trialuser@Yozus-iMac.local>
2022-12-07 15:55:35 +0000

    notifications indicator

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,6 +1,7 @@
 class NotificationsController < ApplicationController
 
     def index
+        current_user.notifications.update_all(viewed: true)
         @notifications = current_user.notifications.order(created_at: :desc)
     end
 end

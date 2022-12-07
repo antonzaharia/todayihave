8174b198aa434dda145d4f4157dd32dd1dfce8be
antonzaharia <trialuser@Yozus-iMac.local>
2022-12-07 10:11:37 +0000

    built the created action for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,20 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+
+      redirect_to dashboard_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :username, :password)
   end
 end

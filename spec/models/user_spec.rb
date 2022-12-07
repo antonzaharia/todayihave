1b49fbc9fd8438340803ecd64f10de512ca1113d
antonzaharia <trialuser@Yozus-iMac.local>
2022-12-07 10:04:18 +0000

    user create method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject(:user) { FactoryBot.build(:user) }
+  subject(:user) { FactoryBot.create(:user) }
 
   it 'should be valid with all attributes' do
     expect(user).to be_valid
+  end
+
+  it 'is not valid without an email' do
+    user.email = nil
+    expect(user).to_not be_valid
+  end
+
+  it 'is not valid without an username' do
+    user.username = nil
+    expect(user).to_not be_valid
+  end
+
+  it 'is not valid without a password' do
+    user.password = nil
+    expect(user).to_not be_valid
+  end
+
+  it 'is not valid with duplicated email' do
+    new_user = FactoryBot.build(:user, email: user.email)
+    expect(new_user).to_not be_valid
+  end
+  
+  it 'is not valid with duplicated username' do
+    new_user = FactoryBot.build(:user, username: user.username)
+    expect(new_user).to_not be_valid
+  end
+
+  it 'is authenticated with correct password' do
+    expect(user.authenticate(user.password)).to eq(user)
   end
 end

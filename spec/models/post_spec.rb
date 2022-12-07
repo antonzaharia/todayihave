d595e7a78f335f744ba4f9624a91405a2ac50582
antonzaharia <trialuser@Yozus-iMac.local>
2022-12-07 11:37:33 +0000

    post unit tests

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,4 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
+  subject(:post) { FactoryBot.create(:post) }
+
+  it 'should be valid with all attributes' do
+    expect(post).to be_valid
+  end
+
+  it 'is not valid without a title' do
+    post.title = nil
+    expect(post).to_not be_valid
+  end
+
+  it 'is not valid without content' do
+    post.content = nil
+    expect(post).to_not be_valid
+  end
+
+  # it 'is not valid without tags' do
+  #   post.tags = []
+  #   expect(post).to_not be_valid
+  #   expect(post.errors.full_messages.first).to eq("Tags can't be blank")
+  # end
+
+  it 'is not valid with duplicated title in the same day' do
+    new_post = FactoryBot.build(:post, title: post.title)
+    expect(new_post).to_not be_valid
+  end
 end

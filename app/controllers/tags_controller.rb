0a5afb1c44917380fb9485ad74e86cbe81fd5e21
antonzaharia <trialuser@Yozus-iMac.local>
2022-12-07 13:38:53 +0000

    tags show & index spec

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,2 +1,10 @@
 class TagsController < ApplicationController
+
+    def show
+        @tag = Tag.find(params[:id])
+    end
+
+    def index
+        @tags = Tag.ordered_by_posts
+    end
 end
--- spec/requests/tags_spec.rb
+++ spec/requests/tags_spec.rb
@@ -1,7 +1,40 @@
 require 'rails_helper'
+require_relative '../helpers.rb'
 
-RSpec.describe "Tags", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Tags', type: :request do
+  describe 'GET /tags/:id' do
+    let(:tag) { FactoryBot.create(:tag) }
+    let(:first_post) { FactoryBot.create(:post) }
+    let(:second_post) { FactoryBot.create(:post) }
+
+    let(:user) { FactoryBot.create(:user) }
+    send_signin_request
+
+    it 'lists all posts that has the specific tag' do
+      first_post.tags << tag
+      second_post.tags << tag
+      get "/tags/#{tag.id}"
+
+      expect(assigns(:tag).posts.size).to eq(2)
+    end
+  end
+
+  describe 'GET /tags' do
+    let(:tag) { FactoryBot.create(:tag) }
+    let(:second_tag) { FactoryBot.create(:tag) }
+    let(:first_post) { FactoryBot.create(:post) }
+    let(:second_post) { FactoryBot.create(:post) }
+
+    let(:user) { FactoryBot.create(:user) }
+    send_signin_request
+
+    it 'lists all posts that has the specific tag' do
+      first_post.tags << tag
+      second_post.tags << tag
+      get "/tags"
+
+      expect(response.body).to include('2 posts')
+      expect(response.body).to include('1 post')
+    end
   end
 end

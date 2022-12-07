ec40ba051bbda547aebb730b6f39cc01b5b53a5a
antonzaharia <trialuser@Yozus-iMac.local>
2022-12-07 11:37:16 +0000

    post resource generated

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,4 +1,11 @@
 FactoryBot.define do
+  factory :post do
+    title { Faker::Lorem.sentence }
+    content { Faker::Lorem.paragraph }
+
+    association :user, factory: :user
+  end
+
   factory(:user) do
     email { Faker::Internet.email }
     password { Faker::Internet.password }

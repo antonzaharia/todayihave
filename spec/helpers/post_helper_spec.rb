902dc7fceadc1c4a98f466401c426bc9c60134d2
antonzaharia <trialuser@Yozus-iMac.local>
2022-12-07 14:52:58 +0000

    post parser for mentions helper specs

--- spec/helpers/post_helper_spec.rb
+++ spec/helpers/post_helper_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require "rails_helper"
 
 # Specs in this file have access to a helper object that includes
 # the TagsHelper. For example:
@@ -11,5 +11,15 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe PostsHelper, type: :helper do
-    pending "add some examples (or delete) #{__FILE__}"
+  RSpec.describe PostsHelper, type: :helper do
+    describe "#parse_content_at_mentions" do
+      before do
+        FactoryBot.create(:user, username: "test")
+      end
+
+      it "it parses the mentions correctly" do
+        expect(helper.parse_content_at_mentions("I mention @test in this post")).to eq("I mention <a style=\"text-decoration: underline; color: hsl(var(--p));\" href=\"/users.#{User.last.id}\">@test</a> in this post")
+      end
+    end
+  end
 end

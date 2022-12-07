b5e7a81446cd11de0363d60872294966d95a92ea
antonzaharia <trialuser@Yozus-iMac.local>
2022-12-07 10:14:23 +0000

    added tests for user authentication

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -8,4 +8,31 @@ RSpec.describe "Sessions", type: :request do
     end
   end
 
+  describe 'POST /sessions' do
+    let(:user) { FactoryBot.create(:user) }
+    let(:params) do
+      {
+        user: {
+          email: user.email,
+          password: user.password
+        }
+      }
+    end
+
+    it 'authenticates the user' do
+      post '/sessions', params: params
+
+      expect(session[:user_id]).to eq(user.id)
+    end
+
+    it 'shows errors if credentials are wrong' do
+      params[:user][:password] = 'wrongpassword'
+      post '/sessions', params: params
+
+      expect(response).to have_http_status(:unauthorized)
+      expect(session[:user_id]).to eq(nil)
+      expect(flash[:message]).to eq('Wrong credentials')
+    end
+  end
+
 end

5c5c81114bdc909610f7a78fdad111a22bc12180
antonzaharia <trialuser@Yozus-iMac.local>
2022-12-07 15:16:24 +0000

    fixed post helper test

--- spec/helpers/post_helper_spec.rb
+++ spec/helpers/post_helper_spec.rb
@@ -1,25 +1,13 @@
 require "rails_helper"
 
-# Specs in this file have access to a helper object that includes
-# the TagsHelper. For example:
-#
-# describe TagsHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe PostsHelper, type: :helper do
-  RSpec.describe PostsHelper, type: :helper do
-    describe "#parse_content_at_mentions" do
-      before do
-        FactoryBot.create(:user, username: "test")
-      end
+  describe "#parse_content_at_mentions" do
+    before do
+      FactoryBot.create(:user, username: "test")
+    end
 
-      it "it parses the mentions correctly" do
-        expect(helper.parse_content_at_mentions("I mention @test in this post")).to eq("I mention <a style=\"text-decoration: underline; color: hsl(var(--p));\" href=\"/users.#{User.last.id}\">@test</a> in this post")
-      end
+    it "it parses the mentions correctly" do
+      expect(helper.parse_content_at_mentions("I mention @test in this post")).to eq("I mention <a style=\"text-decoration: underline; color: hsl(var(--p));\" href=\"/users.#{User.last.id}\">@test</a> in this post")
     end
   end
 end

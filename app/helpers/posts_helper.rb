d6ac2b3e5fc29231bf03e746f980d6a75beb4b4a
antonzaharia <trialuser@Yozus-iMac.local>
2022-12-07 14:53:21 +0000

    uset the post mentions helper

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,13 @@
 module PostsHelper
+  def parse_content_at_mentions(text)
+    text.gsub(/@([a-zA-Z0-9_]+)/) do |match|
+      user = User.find_by(username: Regexp.last_match(1))
+
+      if user.present?
+        link_to(match, users_path(user), style: "text-decoration: underline; color: hsl(var(--p));")
+      else
+        match
+      end
+    end
+  end
 end
